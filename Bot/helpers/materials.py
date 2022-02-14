9d6216a188ec5ac125956dc715fd1ae08e1cca65
BiniTech <binitechnology@gmail.com>
2022-02-14 16:30:53 +0300

    State cancelation updated

--- Bot/helpers/materials.py
+++ Bot/helpers/materials.py
@@ -4,8 +4,7 @@ from Bot.helpers import strings
 
 
 def materials(lang=0, start=0, end=10, i=1):
-    mat = list(CsFile().get())
-    courses = mat[start:end]
+    courses = list(CsFile().get())[start:end]
     txt = []
     btn = InlineKeyboardMarkup(row_width=5)
     for x in courses:
@@ -18,7 +17,7 @@ def materials(lang=0, start=0, end=10, i=1):
     TEXT = f"📚Materials\n\n{TEXT}"
     if start >= 10:
         btn.add(InlineKeyboardButton(strings.backBtnString[lang],
-                                     callback_data=f"pageBar_back_{start - 10}_{end - 10}_{i - 10 - len(mat)}"))
+                                     callback_data=f"pageBar_back_{start - 10}_{end - 10}_{i - 10 - len(courses)}"))
     if end < len(list(CsFile().get())):
         btn.insert(InlineKeyboardButton(strings.nextBtnString[lang],
                                         callback_data=f"pageBar_next_{start + 10}_{end + 10}_{i}"))
